925edac91a11007ffcc361676bf42a9e2574a418
Rubi <57335689+rubiibhandarii@users.noreply.github.com>
2024-10-08 09:56:36 +1100

    adding scenes

---
+++ Assets/SceneFirst.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFirst : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
+
+   
+
